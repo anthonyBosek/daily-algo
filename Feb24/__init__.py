604804d3eb4c3b77100c1016799765502dc8fe3b
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-08 15:41:02 -0700

    my solution

--- Feb24/__init__.py
+++ Feb24/__init__.py
@@ -1,3 +1,12 @@
+"""
+    938. Range Sum of BST
+    https://leetcode.com/problems/range-sum-of-bst/
+
+    Given the root node of a binary search tree and two integers low and high,
+        return the sum of values of all nodes with a value in the inclusive
+        range [low, high].
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
