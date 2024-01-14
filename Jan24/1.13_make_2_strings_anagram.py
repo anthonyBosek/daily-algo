ced55b8564a23c5389c13c12eaa9bf05076bd0af
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-13 17:25:56 -0700

    another one

--- Jan24/1.13_make_2_strings_anagram.py
+++ Jan24/1.13_make_2_strings_anagram.py
@@ -11,17 +11,36 @@
 
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        count_s = [0] * 26
-        count_t = [0] * 26
+        # count_s = [0] * 26
+        # count_t = [0] * 26
 
-        for char in s:
-            count_s[ord(char) - ord("a")] += 1
+        # for char in s:
+        #     count_s[ord(char) - ord("a")] += 1
 
-        for char in t:
-            count_t[ord(char) - ord("a")] += 1
+        # for char in t:
+        #     count_t[ord(char) - ord("a")] += 1
 
-        steps = 0
-        for i in range(26):
-            steps += abs(count_s[i] - count_t[i])
+        # steps = 0
+        # for i in range(26):
+        #     steps += abs(count_s[i] - count_t[i])
 
-        return steps // 2
+        # return steps // 2
+
+        smp = {}
+        tmp = {}
+        cnt = 0
+
+        for a in s:
+            smp[a] = smp.get(a, 0) + 1
+
+        for a in t:
+            tmp[a] = tmp.get(a, 0) + 1
+
+        for key, value in smp.items():
+            if key in tmp:
+                if value == tmp[key]:
+                    cnt += value
+                else:
+                    cnt += min(value, tmp[key])
+
+        return len(s) - cnt
