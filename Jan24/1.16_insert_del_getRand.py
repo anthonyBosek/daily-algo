1d73a50a42ad4541206edfc76391cac7e6c5cb27
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-15 01:30:14 -0700

    my solution and some others

--- Jan24/1.16_insert_del_getRand.py
+++ Jan24/1.16_insert_del_getRand.py
@@ -17,23 +17,97 @@ import random
 
 
 class RandomizedSet:
+    # ------------------------------------------------------------
+    # marksphilip
+    # def __init__(self):
+    #     self.lst = []
+    #     self.idx_map = {}
+
+    # def search(self, val):
+    #     return val in self.idx_map
+
+    # def insert(self, val):
+    #     if self.search(val):
+    #         return False
+
+    #     self.lst.append(val)
+    #     self.idx_map[val] = len(self.lst) - 1
+    #     return True
+
+    # def remove(self, val):
+    #     if not self.search(val):
+    #         return False
+
+    #     idx = self.idx_map[val]
+    #     self.lst[idx] = self.lst[-1]
+    #     self.idx_map[self.lst[-1]] = idx
+    #     self.lst.pop()
+    #     del self.idx_map[val]
+    #     return True
+
+    # def getRandom(self):
+    #     return random.choice(self.lst)
+
+    # ------------------------------------------------------------
+    # my solution
+    # def __init__(self):
+    #     self.my_set = set()
+
+    # def insert(self, val: int) -> bool:
+    #     if val not in self.my_set:
+    #         self.my_set.add(val)
+    #         return True
+    #     return False
+
+    # def remove(self, val: int) -> bool:
+    #     if val in self.my_set:
+    #         self.my_set.remove(val)
+    #         return True
+    #     return False
+
+    # def getRandom(self) -> int:
+    #     return random.choice(list(self.my_set))
+
+    # ------------------------------------------------------------
+    # tolujimoh
     def __init__(self):
-        self.my_set = set()
+        """
+        Initialize your data structure here.
+        """
+        self.hashTable = {}
+        self.arr = []
 
     def insert(self, val: int) -> bool:
-        if val not in self.my_set:
-            self.my_set.add(val)
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.hashTable:
+            self.hashTable[val] = len(self.arr)
+            self.arr.append(val)
             return True
+
         return False
 
     def remove(self, val: int) -> bool:
-        if val in self.my_set:
-            self.my_set.remove(val)
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.hashTable:
+            self.hashTable[self.arr[-1]] = self.hashTable[val]
+            self.arr[self.hashTable[val]] = self.arr[-1]
+
+            self.arr.pop()
+            self.hashTable.pop(val)
+
             return True
+
         return False
 
     def getRandom(self) -> int:
-        return random.sample(self.my_set, 1)[0]
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.arr)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
