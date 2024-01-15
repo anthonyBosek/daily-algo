f11e487492d604ec725c7974483e83d4a74a1645
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-14 23:16:13 -0700

    first attempt

--- Jan24/1.16_insert_del_getRand.py
+++ Jan24/1.16_insert_del_getRand.py
@@ -13,19 +13,27 @@
 
 """
 
+import random
+
 
 class RandomizedSet:
     def __init__(self):
-        pass
+        self.my_set = set()
 
     def insert(self, val: int) -> bool:
-        pass
+        if val not in self.my_set:
+            self.my_set.add(val)
+            return True
+        return False
 
     def remove(self, val: int) -> bool:
-        pass
+        if val in self.my_set:
+            self.my_set.remove(val)
+            return True
+        return False
 
     def getRandom(self) -> int:
-        pass
+        return random.sample(self.my_set, 1)[0]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
