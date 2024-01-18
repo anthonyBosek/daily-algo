844b7c1935164fc54915d8816055b21e5fa236a4
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-17 17:18:24 -0700

    couple of solutions

--- Jan24/1.17_num_of_occurrences.py
+++ Jan24/1.17_num_of_occurrences.py
@@ -11,22 +11,26 @@
 
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        # my solution
+        freq = {}
+        for x in arr:
+            freq[x] = freq.get(x, 0) + 1
+
+        return len(freq) == len(set(freq.values()))
 
         # ------------------------------------------------------------
-        # copilot solution
-        my_set = set()
-        my_dict = {}
-        for num in arr:
-            if num in my_dict:
-                my_dict[num] += 1
-            else:
-                my_dict[num] = 1
 
-        for key in my_dict:
-            if my_dict[key] in my_set:
-                return False
-            else:
-                my_set.add(my_dict[key])
+        # my_set = set()
+        # my_dict = {}
+        # for num in arr:
+        #     if num in my_dict:
+        #         my_dict[num] += 1
+        #     else:
+        #         my_dict[num] = 1
 
-        return True
+        # for key in my_dict:
+        #     if my_dict[key] in my_set:
+        #         return False
+        #     else:
+        #         my_set.add(my_dict[key])
+
+        # return True
