3beae29343daf9c21a9a92f3081e39d00ef3cb67
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-20 15:06:02 -0700

    working solution

--- Jan24/1.20_sum_subArr_mins.py
+++ Jan24/1.20_sum_subArr_mins.py
@@ -10,4 +10,65 @@
 
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        pass
+        # 1. Brute Force
+        """
+        T(n) = O(n)
+        S(n) = O(n)
+        """
+        MOD = 10**9 + 7
+        n = len(arr)
+        left, right = [0] * n, [0] * n
+        stack = []
+        for i in range(n):
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
+            left[i] = stack[-1] if stack else -1
+            stack.append(i)
+        stack = []
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            right[i] = stack[-1] if stack else n
+            stack.append(i)
+        return sum((i - left[i]) * (right[i] - i) * arr[i] for i in range(n)) % MOD
+
+        # 2. Stack
+        # """
+        # T(n) = O(n)
+        # S(n) = O(n)
+        # """
+        # MOD = 10**9 + 7
+        # n = len(arr)
+        # stack = []
+        # res = 0
+        # for i in range(n):
+        #     count = 1
+        #     while stack and stack[-1][0] >= arr[i]:
+        #         val, c = stack.pop()
+        #         count += c
+        #         res -= val * c
+        #     stack.append((arr[i], count))
+        #     res += arr[i] * count
+        # return res % MOD
+
+        # 3. DP
+        # """
+        # T(n) = O(n)
+        # S(n) = O(n)
+        # """
+        # MOD = 10**9 + 7
+        # n = len(arr)
+        # left, right = [0] * n, [0] * n
+        # stack = []
+        # for i in range(n):
+        #     while stack and arr[stack[-1]] > arr[i]:
+        #         stack.pop()
+        #     left[i] = stack[-1] if stack else -1
+        #     stack.append(i)
+        # stack = []
+        # for i in range(n - 1, -1, -1):
+        #     while stack and arr[stack[-1]] >= arr[i]:
+        #         stack.pop()
+        #     right[i] = stack[-1] if stack else n
+        #     stack.append(i)
+        # return sum((i - left[i]) * (right[i] - i) * arr[i] for i in range(n)) % MOD
