5a32d82de0783347a7084a08aefb682a5f858e21
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-21 12:37:51 -0700

    solution for the day

--- Jan24/1.21_house_robber.py
+++ Jan24/1.21_house_robber.py
@@ -17,4 +17,36 @@
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        pass
+        # """
+        #     Time Complexity: O(n)
+        #     Space Complexity: O(1)
+        # """
+        # if not nums:
+        #     return 0
+
+        # prev1 = 0
+        # prev2 = 0
+
+        # for num in nums:
+        #     temp = prev1
+        #     prev1 = max(prev2 + num, prev1)
+        #     prev2 = temp
+
+        # return prev1
+
+        # ---------------------------------------------------------
+
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        if not nums:
+            return 0
+
+        dp = [0] * (len(nums) + 1)
+        dp[1] = nums[0]
+
+        for i in range(2, len(nums) + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+
+        return dp[-1]
