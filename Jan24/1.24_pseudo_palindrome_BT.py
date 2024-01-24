27527fe526c2dd7a5a31796f127731b003e182b4
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-24 15:23:54 -0700

    set up for wed jan 24

---
+++ Jan24/1.24_pseudo_palindrome_BT.py
@@ -0,0 +1,22 @@
+"""
+    1457. Pseudo-Palindromic Paths in a Binary Tree
+    https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/
+
+    Given a binary tree where node values are digits from 1 to 9.
+    A path in the binary tree is said to be pseudo-palindromic
+    if at least one permutation of the node values in the path is a palindrome.
+
+    Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
+        pass
