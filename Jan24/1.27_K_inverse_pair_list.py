13a1c6131a5d5f62fd3b82a5229a0bc81b8ccc1a
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-27 12:44:37 -0700

    inverse pair arrays

---
+++ Jan24/1.27_K_inverse_pair_list.py
@@ -0,0 +1,15 @@
+"""
+    629. K Inverse Pairs Array
+    https://leetcode.com/problems/k-inverse-pairs-array/
+
+    For an integer array nums, an inverse pair is a pair of integers [i, j] where 0 <= i < j < nums.length and nums[i] > nums[j].
+
+    Given two integers n and k, return the number of different arrays consist of numbers from 1 to n such that there are exactly k inverse pairs.
+    Since the answer can be huge, return it modulo 109 + 7.
+
+"""
+
+
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        pass
