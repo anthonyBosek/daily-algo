85476df030824919407717b4fe5e8fe6f27ac0f3
Anthony Bosek <anthony.bosek@gmail.com>
2024-01-30 12:04:00 -0700

    works on algo setup

---
+++ Jan24/1.30_rev_polish_not.py
@@ -0,0 +1,14 @@
+"""
+    150. Evaluate Reverse Polish Notation
+    https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+    Evaluate the expression. Return an integer that represents the value of the expression.
+
+"""
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        pass
