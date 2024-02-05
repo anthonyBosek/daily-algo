bb10c48815cd275bdfd50b90006373282eed9336
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-05 13:11:39 -0700

    add .py file - daily challenge feb 5th

---
+++ Feb24/2.5_first_unique_char.py
@@ -0,0 +1,12 @@
+"""
+    387. First Unique Character in a String
+    https://leetcode.com/problems/first-unique-character-in-a-string/
+
+    Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+"""
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        pass
