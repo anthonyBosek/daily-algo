30056a5b105272fe0fd53d77f82bacd15f75513c
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-06 17:41:22 -0700

    add dir 2.6 daily challenge

---
+++ Feb24/2.6_group_anagrams.py
@@ -0,0 +1,15 @@
+"""
+    49. Group Anagrams
+    https://leetcode.com/problems/group-anagrams/
+
+    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+        typically using all the original letters exactly once.
+
+"""
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        pass
