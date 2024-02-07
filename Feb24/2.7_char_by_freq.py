a8e40d41eb1808392e37da275dc7d20350129ebe
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-07 08:11:02 -0700

    create dir daily algo feb 7

---
+++ Feb24/2.7_char_by_freq.py
@@ -0,0 +1,16 @@
+"""
+    451. Sort Characters By Frequency
+    https://leetcode.com/problems/sort-characters-by-frequency/
+
+    Given a string s, sort it in decreasing order based on the frequency of the characters.
+    
+    The frequency of a character is the number of times it appears in the string.
+
+    Return the sorted string. If there are multiple answers, return any of them.
+
+"""
+
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        pass
