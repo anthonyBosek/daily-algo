f533b6846329e3272f118b0759777c03697987ba
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-07 10:32:03 -0700

    add working solution

--- Feb24/2.7_char_by_freq.py
+++ Feb24/2.7_char_by_freq.py
@@ -11,6 +11,53 @@
 """
 
 
-class Solution:
-    def frequencySort(self, s: str) -> str:
-        pass
+def frequencySort(s):
+    # my solution
+    #
+    # creat dict for character frequency
+    freq = {}
+
+    # iterate through string and increment frequency count
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    # use dict.items to create (key,value) tuple and sort by frequencies - descending
+    s_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+    # concatenate charcters onto result string
+    result = ""
+    for char, count in s_freq:
+        result += char * count
+
+    return result
+
+    # ---------------------------------------------------------------
+
+    # copilot solution
+    # freq = {}
+    # for c in s:
+    #     if c in freq:
+    #         freq[c] += 1
+    #     else:
+    #         freq[c] = 1
+
+    # sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    # result = ""
+    # for c, count in sorted_freq:
+    #     result += c * count
+
+    # return result
+
+    # ---------------------------------------------------------------
+
+    # s = sorted(s, reverse=True)
+    # return "".join(sorted(s, key=lambda k: s.count(k), reverse=True))
+
+
+print(frequencySort("tree"))  # "eert"
+print(frequencySort("cccaaa"))  # "cccaaa"
+print(frequencySort("Aabb"))  # "bbAa"
+print(frequencySort("loveleetcode"))  # "eeeeoollvtdc"
+print(frequencySort("raaeaedere"))  # "eeeeaaarrd"
