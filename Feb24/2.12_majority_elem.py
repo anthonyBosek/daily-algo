a5969481558ca3afe90dcb6ae66e4b6b051775c7
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-12 16:16:36 -0700

    add multiple working solutions

--- Feb24/2.12_majority_elem.py
+++ Feb24/2.12_majority_elem.py
@@ -12,4 +12,36 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        pass
+        # Using Boyer-Moore Voting Algorithm
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        return candidate
+
+        # ---------------------------------------------
+
+        # Using Counter
+        # from collections import Counter
+        # count = Counter(nums)
+        # return max(count.keys(), key=count.get)
+
+        # ---------------------------------------------
+
+        # Using Sorting
+        # nums.sort()
+        # return nums[len(nums)//2]
+
+        # ---------------------------------------------
+        # Using Set
+        # return max(set(nums), key=nums.count)
+
+        # ---------------------------------------------
+
+        # Using Dictionary
+        # count = {}
+        # for num in nums:
+        #     count[num] = count.get(num, 0) + 1
+        # return max(count.keys(), key=count.get)
