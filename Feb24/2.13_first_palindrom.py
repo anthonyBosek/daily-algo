1e75823c2f61fdb40d9f0065d76070d317c96417
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-13 11:38:02 -0700

    add feb 13 challenge

---
+++ Feb24/2.13_first_palindrom.py
@@ -0,0 +1,15 @@
+"""
+    2108. Find First Palindromic String in the Array
+    https://binarysearch.com/problems/Find-First-Palindromic-String-in-the-Array
+
+    Given an array of strings words, return the first palindromic string in the array.
+        If there is no such string, return an empty string "".
+
+    A string is palindromic if it reads the same forward and backward.
+
+"""
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        pass
