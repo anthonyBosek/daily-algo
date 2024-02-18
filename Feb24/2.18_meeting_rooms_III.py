cc3213b16c3ee0583185b44c7f212f2992870dee
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-18 15:38:16 -0700

    add working solution

--- Feb24/2.18_meeting_rooms_III.py
+++ Feb24/2.18_meeting_rooms_III.py
@@ -20,7 +20,62 @@
 
 """
 
+from heapq import heappop, heappush
+from operator import itemgetter
+
 
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        pass
+        available_rooms = list(range(n))
+        used_rooms = []
+        meetings_per_room = [0] * n
+
+        for start, end in sorted(meetings):
+            while used_rooms and used_rooms[0][0] <= start:
+                _, room = heappop(used_rooms)
+                heappush(available_rooms, room)
+
+            if available_rooms:
+                room = heappop(available_rooms)
+                heappush(used_rooms, (end, room))
+            else:
+                room_end, room = heappop(used_rooms)
+                room_end += end - start
+                heappush(used_rooms, (room_end, room))
+            meetings_per_room[room] += 1
+
+        max_index, max_value = max(
+            enumerate(meetings_per_room), key=operator.itemgetter(1)
+        )
+        return max_index
+
+    # -----------------------------------------------------------------------
+
+    # ans = [0] * n
+    # times = [0] * n
+    # meetings.sort()
+
+    # for start, end in meetings:
+    #     flag = False
+    #     minind = -1
+    #     val = float('inf')
+    #     for j in range(n):
+    #         if times[j] < val:
+    #             val = times[j]
+    #             minind = j
+    #         if times[j] <= start:
+    #             flag = True
+    #             ans[j] += 1
+    #             times[j] = end
+    #             break
+    #     if not flag:
+    #         ans[minind] += 1
+    #         times[minind] += (end - start)
+
+    # maxi = -1
+    # id = -1
+    # for i in range(n):
+    #     if ans[i] > maxi:
+    #         maxi = ans[i]
+    #         id = i
+    # return id
