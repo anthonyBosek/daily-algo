997293e8dd7f3c5a6851d3eb84eb21000f5994fc
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-22 16:20:39 -0700

    working solution

--- Feb24/2.22_town_judge.py
+++ Feb24/2.22_town_judge.py
@@ -20,4 +20,29 @@
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        pass
+        # two dict example
+        # trust_to, trusted = defaultdict(int), defaultdict(int)
+
+        # for a, b in trust:
+        #     print(a, b)
+        #     trust_to[a] += 1
+        #     trusted[b] += 1
+
+        # for i in range(1, n+1):
+        #     if trust_to[i] == 0 and trusted[i] == n - 1:
+        #         return i
+
+        # return -1
+
+        # tabulation list example
+        trust_count = [0] * (n + 1)
+
+        for a, b in trust:
+            trust_count[a] -= 1
+            trust_count[b] += 1
+
+        for i in range(1, n + 1):
+            if trust_count[i] == n - 1:
+                return i
+
+        return -1
