4d5e4d4d53e39f3d2431d16de36c1d79977b1c37
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-28 12:33:16 -0700

    add branch for feb 28 algo challenge

---
+++ Feb24/2.28_bottom_left.py
@@ -0,0 +1,19 @@
+"""
+    513. Find Bottom Left Tree Value
+    https://leetcode.com/problems/find-bottom-left-tree-value/
+
+    Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        pass
