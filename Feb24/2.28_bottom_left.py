fbf511c9bcb0779c01d9436f918d832be42e2516
Anthony Bosek <anthony.bosek@gmail.com>
2024-02-28 12:40:06 -0700

    working solution

--- Feb24/2.28_bottom_left.py
+++ Feb24/2.28_bottom_left.py
@@ -6,6 +6,8 @@
 
 """
 
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -16,4 +18,47 @@
 
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        pass
+        left = root
+        q = deque([root])
+
+        while q:
+            rowLen = len(q)
+            for i in range(rowLen):
+                n = q.popleft()
+                if n:
+                    if n.left:
+                        q.append(n.left)
+                    if n.right:
+                        q.append(n.right)
+                    if i == 0:
+                        left = n
+        return left.val
+
+        # ---------------------------------------------------------
+
+        # if root is None:
+        #     return None
+
+        # queue = [root]
+        # while queue:
+        #     size = len(queue)
+        #     for i in range(size):
+        #         node = queue.pop(0)
+        #         if i == 0:
+        #             leftmost = node.val
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        # return leftmost
+
+        # ---------------------------------------------------------
+
+        # queue = [root]
+        # while queue:
+        #     node = queue.pop(0)
+        #     if node.right:
+        #         queue.append(node.right)
+        #     if node.left:
+        #         queue.append(node.left)
+        # return node.val
