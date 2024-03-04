0bfc2c71e5bcf6aa2dee5d470118e98a7f4b8329
Anthony Bosek <anthony.bosek@gmail.com>
2024-03-04 16:24:15 -0700

    add working solution

--- Mar24/3.4_bag_o_tokens.py
+++ Mar24/3.4_bag_o_tokens.py
@@ -15,7 +15,68 @@
 
 """
 
+from collections import deque
+
 
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        pass
+        score = 0
+        tokens.sort()
+        deque = deque(tokens)
+        while deque:
+            if deque and power >= deque[0]:
+                power -= deque.popleft()
+                score += 1
+
+            else:
+                if len(deque) > 2 and score:
+                    power += deque.pop()
+                    score -= 1
+                else:
+                    return score
+
+        return score
+
+        # -----------------------------------------------------------
+
+        # tokens.sort()
+        # score = 0
+        # max_score = 0
+        # left = 0
+        # right = len(tokens) - 1
+
+        # while left <= right:
+        #     if power >= tokens[left]:
+        #         power -= tokens[left]
+        #         left += 1
+        #         score += 1
+        #         max_score = max(max_score, score)
+        #     elif score > 0:
+        #         power += tokens[right]
+        #         right -= 1
+        #         score -= 1
+        #     else:
+        #         break
+
+        # return max_score
+
+        # -----------------------------------------------------------
+
+        # tokens.sort()
+        # score = 0
+        # max_score = 0
+        # i = 0
+        # j = len(tokens) - 1
+        # while i <= j:
+        #     if power >= tokens[i]:
+        #         power -= tokens[i]
+        #         score += 1
+        #         i += 1
+        #         max_score = max(max_score, score)
+        #     elif score > 0:
+        #         power += tokens[j]
+        #         score -= 1
+        #         j -= 1
+        #     else:
+        #         break
+        # return max_score
