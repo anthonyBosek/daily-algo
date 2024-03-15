f0e6786b67b87e4611efb79f8d13989fe15a6cae
Anthony Bosek <anthony.bosek@gmail.com>
2024-03-15 16:32:20 -0700

    adds working solutions

--- Mar24/3.15_product_of_list.py
+++ Mar24/3.15_product_of_list.py
@@ -13,4 +13,40 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        # res = [1]*len(nums)
+        # for i in range(1,len(nums)):
+        #     res[i] = res[i-1] * nums[i-1]
+        # post = 1
+        # for i in range(len(nums)-1,-1,-1):
+        #     res[i] *= post
+        #     post *= nums[i]
+        # return res
+
+        # ---------------------------------------------------
+
+        prefix, postfix = 1, 1
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+
+            res[i] = prefix
+            prefix *= nums[i]
+
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+        # ---------------------------------------------------
+
+        # n = len(nums)
+        # left = [0] * n
+        # right = [0] * n
+        # left[0] = 1
+        # right[n-1] = 1
+        # for i in range(1, n):
+        #     left[i] = left[i-1] * nums[i-1]
+        # for i in range(n-2, -1, -1):
+        #     right[i] = right[i+1] * nums[i+1]
+        # for i in range(n):
+        #     nums[i] = left[i] * right[i]
+        # return nums
