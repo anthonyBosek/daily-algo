8dac4d7a9a6651ba81adbb7a369bfab19b7ed75e
Anthony Bosek <anthony.bosek@gmail.com>
2024-03-28 16:06:46 -0700

    add working solution

--- Mar24/3.28_longest_sub_freq_k.py
+++ Mar24/3.28_longest_sub_freq_k.py
@@ -17,4 +17,37 @@
 
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        pass
+        freq = {}
+        left = 0
+        max = 0
+        for right in range(len(nums)):
+            valright = nums[right]
+            if valright in freq:
+                freq[valright] += 1
+            else:
+                freq[valright] = 1
+            while freq[valright] > k and left <= right:
+                valleft = nums[left]
+                freq[valleft] -= 1
+                left += 1
+            length = right - left + 1
+            if length > max:
+                max = length
+        return max
+
+        # ------------------------------------------------------
+
+        # ans = 0
+        # mp = {}
+        # l = 0
+        # n = len(nums)
+        # for r in range(n):
+        #     mp[nums[r]] = mp.get(nums[r], 0) + 1
+        #     if mp[nums[r]] > k:
+        #         while nums[l] != nums[r]:
+        #             mp[nums[l]] -= 1
+        #             l += 1
+        #         mp[nums[l]] -= 1
+        #         l += 1
+        #     ans = max(ans, r - l + 1)
+        # return ans
