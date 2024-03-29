b577cefd97991f1149ae1d261d99d372d33c4ef8
Anthony Bosek <anthony.bosek@gmail.com>
2024-03-29 11:04:44 -0700

    add working solutions

--- Mar24/3.29_count_subArr_k_times.py
+++ Mar24/3.29_count_subArr_k_times.py
@@ -13,4 +13,32 @@
 
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        pass
+        ma = nums[0]
+        ps = [0]
+        n = len(nums)
+        for i in range(1, n):
+            if nums[i] > ma:
+                ma = nums[i]
+                ps = [i]
+            elif nums[i] == ma:
+                ps.append(i)
+        if len(ps) < k:
+            return 0
+        ans = 0
+        idx = -1
+        for l in range(len(ps) - k + 1):
+            ans += (n - ps[l + k - 1]) * (ps[l] - idx)
+            idx = ps[l]
+        return ans
+
+        # ---------------------------------------------------
+
+        # mx, ans, l, r, n = max(nums), 0, 0, 0, len(nums)
+        # while r < n:
+        #     k -= nums[r] == mx
+        #     r += 1
+        #     while k == 0:
+        #         k += nums[l] == mx
+        #         l += 1
+        #     ans += l
+        # return ans
