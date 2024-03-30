6d6889190a0df971ce88be918ece4c65a5546fcb
Anthony Bosek <anthony.bosek@gmail.com>
2024-03-30 11:08:38 -0700

    add daily branch

---
+++ Mar24/3.30_subArr_k_diff_int.py
@@ -0,0 +1,18 @@
+"""
+    992. Subarrays with K Different Integers
+    https://leetcode.com/problems/subarrays-with-k-different-integers/
+
+    Given an integer array nums and an integer k, return the number of good subarrays of nums.
+
+    A good array is an array where the number of different integers in that array is exactly k.
+
+    • For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+
+    A subarray is a contiguous part of an array.
+
+"""
+
+
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        pass
