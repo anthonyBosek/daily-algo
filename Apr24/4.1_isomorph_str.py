daf54b7246142e4bbc9768eca530327ce4a8404c
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-02 12:14:15 -0700

    add working solution

--- Apr24/4.1_isomorph_str.py
+++ Apr24/4.1_isomorph_str.py
@@ -14,4 +14,47 @@
 
 
 def isIsomorphic(s, t):
-    pass
+    if len(s) != len(t):
+        return False
+    s = list(s)
+    t = list(t)
+    if len(set(s)) != len(set(t)):
+        return False
+    d1 = {}
+    for i in range(len(s)):
+        d1[s[i]] = t[i]
+    print(d1)
+    for i in range(len(s)):
+        s[i] = d1[s[i]]
+    return True if s == t else False
+
+    # ----------------------------------------------
+
+    # map = {}
+    # map2 = {}
+    # for i in range(len(s)):
+    #     if s[i] in map:
+    #         if map[s[i]] != t[i]:
+    #             return False
+    #     if t[i] in map2:
+    #         if map2[t[i]] != s[i]:
+    #             return False
+    #     else:
+    #         map[s[i]] = t[i]
+    #         map2[t[i]] = s[i]
+    # return True
+
+    # ----------------------------------------------
+
+    # if len(s) != len(t):
+    #     return False
+    # s_dict = {}
+    # t_dict = {}
+    # for i in range(len(s)):
+    #     if s[i] not in s_dict:
+    #         s_dict[s[i]] = t[i]
+    #     if t[i] not in t_dict:
+    #         t_dict[t[i]] = s[i]
+    #     if s_dict[s[i]] != t[i] or t_dict[t[i]] != s[i]:
+    #         return False
+    # return True
