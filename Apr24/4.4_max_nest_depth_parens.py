4c31170aa7b78adbdc64382a9494c680d22199d7
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-04 16:56:51 -0700

    add working solution

--- Apr24/4.4_max_nest_depth_parens.py
+++ Apr24/4.4_max_nest_depth_parens.py
@@ -18,4 +18,37 @@
 
 class Solution:
     def maxDepth(self, s: str) -> int:
-        pass
+        res = 0
+        depth = 0
+        for c in s:
+            if c == "(":
+                depth += 1
+                res = max(res, depth)
+            elif c == ")":
+                depth -= 1
+        return res
+
+        # ------------------------------------------
+
+        # count=0
+        # max1 =0
+        # for i in s:
+        #     if i == '(':
+        #         count+=1
+        #         if count > max1:
+        #             max1 = count
+        #     if i == ')':
+        #         count-=1
+        # return max1
+
+        # ------------------------------------------
+
+        # max_depth = 0
+        # depth = 0
+        # for c in s:
+        #     if c == '(':
+        #         depth += 1
+        #         max_depth = max(max_depth, depth)
+        #     elif c == ')':
+        #         depth -= 1
+        # return max_depth
