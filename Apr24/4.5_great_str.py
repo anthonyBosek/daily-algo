4e70847fd7724e5077c2a0475854e635e52f75c8
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-05 15:14:58 -0700

    add working solution

--- Apr24/4.5_great_str.py
+++ Apr24/4.5_great_str.py
@@ -17,7 +17,28 @@
 
 """
 
+from collections import deque
 
-class Solution:
-    def makeGood(self, s: str) -> str:
-        pass
+
+def makeGood(self, s: str) -> str:
+    # n = len(s)
+    # s = deque(s)
+    # stk = []
+    # while s:
+    #     stk.append(s.popleft())
+    #     if len(stk) >= 2 and (
+    #         stk[-1] != stk[-2] and stk[-1].lower() == stk[-2].lower()
+    #     ):
+    #         stk[:] = stk[:-2]
+
+    # return "".join(stk)
+
+    # --------------------------------------------------
+
+    stack = []
+    for c in s:
+        if stack and abs(ord(stack[-1]) - ord(c)) == 32:
+            stack.pop()
+        else:
+            stack.append(c)
+    return "".join(stack)
