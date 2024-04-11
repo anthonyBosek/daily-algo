272616a51d4539952ca8c97cda6091c8e6594e1a
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-11 16:32:51 -0700

    add working solution

--- Apr24/4.11_rem_k_digits.py
+++ Apr24/4.11_rem_k_digits.py
@@ -9,4 +9,38 @@
 
 
 def removeKdigits(num, k):
-    pass
+    if len(num) == k:
+        return "0"
+
+    stack = []
+
+    for digit in num:
+        while k > 0 and stack and stack[-1] > digit:
+            stack.pop()
+            k -= 1
+        stack.append(digit)
+
+    # If k is still greater than 0, remove remaining digits from the end
+    while k > 0:
+        stack.pop()
+        k -= 1
+
+    # Remove leading zeros
+    while stack and stack[0] == "0":
+        stack.pop(0)
+
+    # If stack is empty, return "0"
+    if not stack:
+        return "0"
+
+    return "".join(stack)
+
+    # --------------------------------------------------------------------------------
+
+    # stack = []
+    # for digit in num:
+    #     while k and stack and stack[-1] > digit:
+    #         stack.pop()
+    #         k -= 1
+    #     stack.append(digit)
+    # return "".join(stack[: -k or None]).lstrip("0") or "0"
