be35818d46f0e60aef73e1555ed42313ee4e3c5d
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-14 09:25:03 -0700

    add daily branch

---
+++ Apr24/4.14_sum_left_leaves.py
@@ -0,0 +1,20 @@
+"""
+    404. Sum of Left Leaves
+    https://leetcode.com/problems/sum-of-left-leaves/
+
+    Given the root of a binary tree, return the sum of all left leaves.
+
+    A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        pass
