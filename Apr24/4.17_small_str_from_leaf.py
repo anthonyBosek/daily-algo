13306444eb994b552a8927ea808c2de527f26bfe
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-17 11:24:13 -0700

    add daily algo

---
+++ Apr24/4.17_small_str_from_leaf.py
@@ -0,0 +1,26 @@
+"""
+    988. Smallest String Starting From Leaf
+    https://leetcode.com/problems/smallest-string-starting-from-leaf/
+
+    You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+
+    Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+
+    As a reminder, any shorter prefix of a string is lexicographically smaller.
+
+        - For example, "ab" is lexicographically smaller than "aba".
+
+    A leaf of a node is a node that has no children.
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        pass
