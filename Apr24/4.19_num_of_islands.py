5b066d7dcd8e7e34747081695f856635ff510564
Anthony Bosek <anthony.bosek@gmail.com>
2024-04-19 05:40:14 -0700

    add viable solution

--- Apr24/4.19_num_of_islands.py
+++ Apr24/4.19_num_of_islands.py
@@ -10,7 +10,49 @@
 
 """
 
+from typing import List
+
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        pass
+        if not grid:
+            return 0
+
+        number_of_islands = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    number_of_islands += 1
+
+        return number_of_islands
+
+        # -------------------------------------------------------------------------------------
+
+        # def dfs(grid, i, j):
+        #     if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "0":
+        #         return
+        #     grid[i][j] = "0"
+        #     dfs(grid, i + 1, j)
+        #     dfs(grid, i - 1, j)
+        #     dfs(grid, i, j + 1)
+        #     dfs(grid, i, j - 1)
+
+        # count = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j] == "1":
+        #             count += 1
+        #             dfs(grid, i, j)
+        # return count
