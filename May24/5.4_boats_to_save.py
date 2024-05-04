50690c6a557c4f3eb6c06dae788c08c67acdd951
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-04 13:35:54 -0700

    add solution

--- May24/5.4_boats_to_save.py
+++ May24/5.4_boats_to_save.py
@@ -15,4 +15,12 @@ from typing import List
 
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        pass
+        people.sort()
+        i, j = 0, len(people) - 1
+        boats = 0
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+            boats += 1
+        return boats
