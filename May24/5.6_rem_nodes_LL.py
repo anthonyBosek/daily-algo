0d77e6fb45202913084eeee80ba6e136a26e14fe
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-06 14:11:15 -0700

    add solution

--- May24/5.6_rem_nodes_LL.py
+++ May24/5.6_rem_nodes_LL.py
@@ -22,4 +22,79 @@ class ListNode:
 
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        # optimal
+        # O(N)
+
+        head_rev = self.reverseList(head)
+
+        temp = head_rev
+        max_val = -1
+        prev = None
+        while temp:
+            if temp.val >= max_val:
+                max_val = temp.val
+                prev = temp
+            else:
+                prev.next = temp.next
+            temp = temp.next
+        return self.reverseList(head_rev)
+
+    def reverseList(self, head):
+        prev = None
+        curr = head
+        while curr:
+            curr_next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = curr_next
+        return prev
+
+        # ------------------------------------------------------------------
+
+        # dummy = head
+        # rev = None
+
+        # # reverse the list and store in temp rev
+        # while dummy:
+        #     next_node = dummy.next
+        #     dummy.next = rev
+        #     rev = dummy
+        #     dummy = next_node
+
+        # # iterate over reversed list
+        # dummy = rev
+        # head = None
+        # max_value = float("-inf")
+
+        # # Create new list
+        # while dummy:
+        #     if dummy.val >= max_value:
+        #         max_value = dummy.val
+
+        #         # same logic as for reversing
+        #         new_node = dummy.next
+        #         dummy.next = head
+        #         head = dummy
+        #         dummy = new_node
+        #     else:
+        #         dummy = dummy.next
+
+        # return head
+
+        # ------------------------------------------------------------------
+
+        # cur = head
+        # stack = []
+        # while cur:
+        #     while stack and stack[-1].val < cur.val:
+        #         stack.pop()
+        #     stack.append(cur)
+        #     cur = cur.next
+
+        # nxt = None
+        # while stack:
+        #     cur = stack.pop()
+        #     cur.next = nxt
+        #     nxt = cur
+
+        # return cur
