9ad862b2aa735fb8548c2057c563782208b0e43b
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-07 05:46:01 -0700

    add daily algo

---
+++ May24/5.7_dbl_num_as_LL.py
@@ -0,0 +1,21 @@
+"""
+    2816. Double a Number Represented as a Linked List
+    https://leetcode.com/problems/double-a-number-represented-as-linked-list/
+
+    You are given the head of a non-empty linked list representing a non-negative integer without leading zeroes.
+
+    Return the head of the linked list after doubling it.
+
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
