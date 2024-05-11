808baba81d882f010cb17871aa1b5abbbd9850dd
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-11 09:13:42 -0700

    add viable solution

--- May24/5.11_min_cost_kth_wrkrs.py
+++ May24/5.11_min_cost_kth_wrkrs.py
@@ -20,11 +20,67 @@
 
 """
 
-from typing import List
+# from typing import List
+from heapq import heapify, heapreplace
 
 
-class Solution:
-    def mincostToHireWorkers(
-        self, quality: List[int], wage: List[int], k: int
-    ) -> float:
-        pass
+# class Solution:
+#     def mincostToHireWorkers(
+#         self, quality: List[int], wage: List[int], k: int
+#     ) -> float:
+#         # pairs = sorted(zip(quality, wage), key=lambda p: p[1] / p[0])
+#         # h = [-q for q, _ in pairs[:k]]
+#         # heapify(h)
+#         # sum_q = -sum(h)
+#         # ans = sum_q * pairs[k - 1][1] / pairs[k - 1][0]
+#         # for q, w in pairs[k:]:
+#         #     if q < -h[0]:
+#         #         sum_q += heapreplace(h, -q) + q
+#         #         ans = min(ans, sum_q * w / q)
+#         # return ans
+
+#         # ----------------------------------------------------------------
+
+#         # ratio = sorted([(w / q, q) for w, q in zip(wage, quality)])
+#         # max_heap = []
+#         # quality_sum = 0
+#         # max_ratio = 0.0
+
+#         # for i in range(k):
+#         #     max_ratio = max(max_ratio, ratio[i][0])
+#         #     quality_sum += ratio[i][1]
+#         #     heapq.heappush(max_heap, -ratio[i][1])
+
+#         # res = max_ratio * quality_sum
+
+#         # for i in range(k, len(quality)):
+#         #     max_ratio = max(max_ratio, ratio[i][0])
+#         #     quality_sum += ratio[i][1] + heapq.heappop(max_heap)
+#         #     heapq.heappush(max_heap, -ratio[i][1])
+#         #     res = min(res, max_ratio * quality_sum)
+
+#         # return res
+
+#         # ----------------------------------------------------------------
+
+#         pass
+
+
+class Solution(object):
+    def mincostToHireWorkers(self, quality, wage, k):
+        """
+        :type quality: List[int]
+        :type wage: List[int]
+        :type k: int
+        :rtype: float
+        """
+        qw = sorted(zip(quality, wage), key=lambda p: p[1] / float(p[0]))
+        h = [-q for q, _ in qw[:k]]
+        heapify(h)
+        sum_q = -sum(h)
+        ans = sum_q * qw[k - 1][1] / float(qw[k - 1][0])
+        for q, w in qw[k:]:
+            if q < -h[0]:
+                sum_q += heapreplace(h, -q) + q
+                ans = min(ans, sum_q * w / float(q))
+        return ans
