298ab6ec3293059601d21e1b8a4ab22e3b1174e8
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-11 09:00:53 -0700

    add daily algo challenge

---
+++ May24/5.11_min_cost_kth_wrkrs.py
@@ -0,0 +1,30 @@
+"""
+    857. Minimum Cost to Hire K Workers
+    https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+
+    There are n workers. You are given two integer arrays quality and wage where quality[i]
+        is the quality of the ith worker and wage[i] is the minimum wage expectation for
+        the ith worker.
+
+    We want to hire exactly k workers to form a paid group. To hire a group of k workers,
+        we must pay them according to the following rules:
+
+            1. Every worker in the paid group must be paid at least their minimum wage expectation.
+
+            2. In the group, each worker's pay must be directly proportional to their quality.
+                This means if a worker’s quality is double that of another worker in the group,
+                then they must be paid twice as much as the other worker.
+
+    Given the integer k, return the least amount of money needed to form a paid group satisfying the
+        above conditions. Answers within 10-5 of the actual answer will be accepted.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def mincostToHireWorkers(
+        self, quality: List[int], wage: List[int], k: int
+    ) -> float:
+        pass
