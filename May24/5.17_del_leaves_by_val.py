9af8a1fe56dff993bdebd23f59d0c37277b09464
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-17 10:32:37 -0700

    add solution

--- May24/5.17_del_leaves_by_val.py
+++ May24/5.17_del_leaves_by_val.py
@@ -25,4 +25,51 @@ class Solution:
     def removeLeafNodes(
         self, root: Optional[TreeNode], target: int
     ) -> Optional[TreeNode]:
-        pass
+        if not root:
+            return None
+
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if not root.left and not root.right and root.val == target:
+            return None
+
+        return root
+
+        # -----------------------------------------------------------------------------------------
+        #! Recursive solution with helper func from leetcode
+
+        # # recursive soln for root - if its child is a leaf node AND its child has target, delete
+        # # go up
+        # def helper(node):
+        #     # first go down if necessary
+        #     if not node:
+        #         return
+
+        #     helper(node.left)
+        #     helper(node.right)
+        #     # potentially delete left child
+        #     # if node.val == target:
+        #     # print(f'For node {node.val} we have left {node.left} and right {node.right}')
+        #     if (
+        #         node.left
+        #         and not node.left.left
+        #         and not node.left.right
+        #         and node.left.val == target
+        #     ):
+
+        #         node.left = None
+        #     # potentially delete right child
+        #     if (
+        #         node.right
+        #         and not node.right.left
+        #         and not node.right.right
+        #         and node.right.val == target
+        #     ):
+        #         node.right = None
+
+        # helper(root)
+        # # we have an edge case here where root itself is a leaf and needs deletion
+        # if root.val == target and not root.left and not root.right:
+        #     return None
+        # return root
