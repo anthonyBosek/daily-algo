ed25e405d9bad34f185b9e6ac5dcc893014e1eec
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-18 09:49:13 -0700

    add daily branch

---
+++ May24/5.18_dist_coins_bin_tree.py
@@ -0,0 +1,28 @@
+"""
+    979. Distribute Coins in Binary Tree
+    https://leetcode.com/problems/distribute-coins-in-binary-tree/
+
+    You are given the root of a binary tree with n nodes where each node in the tree has node.val coins.
+        There are n coins in total throughout the whole tree.
+
+    In one move, we may choose two adjacent nodes and move one coin from one node to another.
+        A move may be from parent to child, or from child to parent.
+
+    Return the minimum number of moves required to make every node have exactly one coin.
+
+"""
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        pass
