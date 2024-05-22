373019352900bf7cf5f4e041f6b2a831cbf2e91d
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-22 12:20:17 -0700

    add solution

--- May24/5.22_palindrome_partition.py
+++ May24/5.22_palindrome_partition.py
@@ -13,4 +13,32 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        pass
+        n = len(s)
+        dp = [[] for _ in range(n + 1)]
+        dp[0].append([])
+
+        for i in range(n + 1):
+            for j in range(i):
+                if s[j:i] == s[j:i][::-1]:
+                    for prev in dp[j]:
+                        dp[i].append(prev + [s[j:i]])
+        return dp[n]
+
+    # ? --------------------------------------------------
+    #
+    # --- option 1 ---
+    #
+    #     result = []
+    #     self.dfs(s, [], result)
+    #     return result
+
+    # def dfs(self, s, path, result):
+    #     if not s:
+    #         result.append(path)
+    #         return
+    #     for i in range(1, len(s) + 1):
+    #         if self.is_palindrome(s[:i]):
+    #             self.dfs(s[i:], path + [s[:i]], result)
+
+    # def is_palindrome(self, s):
+    #     return s == s[::-1]
