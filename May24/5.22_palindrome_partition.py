8c633963fb56b43a9dd168020ff5c52dfb5ef007
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-22 12:11:22 -0700

    add daily challenge

---
+++ May24/5.22_palindrome_partition.py
@@ -0,0 +1,16 @@
+"""
+    131. Palindrome Partitioning
+    https://leetcode.com/problems/palindrome-partitioning/
+
+    Given a string s, partition s such that every substring of the partition is a palindrome.
+    
+    Return all possible palindrome partitioning of s.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        pass
