09cc47cc504b52949462bfc96e16bef8d658276d
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-25 07:13:18 -0700

    add daily branch

---
+++ 24.5-May/5.25_word_break_II.py
@@ -0,0 +1,17 @@
+"""
+    140. Word Break II
+    https://leetcode.com/problems/word-break-ii/
+
+    Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence
+        where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+    Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        pass
