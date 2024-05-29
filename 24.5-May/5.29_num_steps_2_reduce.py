dab1d7588f813fbecb1a69cbf54f5e62c9327915
Anthony Bosek <anthony.bosek@gmail.com>
2024-05-29 05:26:30 -0700

    add daily algo

---
+++ 24.5-May/5.29_num_steps_2_reduce.py
@@ -0,0 +1,18 @@
+"""
+    1404. Number of Steps to Reduce a Number in Binary Representation to One
+    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+
+    Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+
+        • If the current number is even, you have to divide it by 2.
+
+        • If the current number is odd, you have to add 1 to it.
+
+    It is guaranteed that you can always reach one for all test cases.
+
+"""
+
+
+class Solution:
+    def numSteps(self, s: str) -> int:
+        pass
