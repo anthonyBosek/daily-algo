9400b94eba4786d92d045a018c07c76cac965222
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-01 11:21:06 -0700

    add solution

--- 24.6-Jun/6.1_3110.py
+++ 24.6-Jun/6.1_3110.py
@@ -12,4 +12,14 @@
 
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        pass
+        score = 0
+        for i in range(1, len(s)):
+            score += abs(ord(s[i]) - ord(s[i - 1]))
+        return score
+
+        #! -------------------------------------------
+
+        # result = 0
+        # for i in range(len(s) - 1):
+        #     result += abs(ord(s[i]) - ord(s[i + 1]))
+        # return result
