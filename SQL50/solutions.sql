56aaf11193090f823cfd7e42bf88ae7962ec166b
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-03 12:05:56 -0700

    set up SQL50 day 4

--- SQL50/solutions.sql
+++ SQL50/solutions.sql
@@ -10,8 +10,11 @@ SELECT name
 FROM Customer
 WHERE referee_id != 2 OR referee_id is null
 
--- Day3
+-- Day 3
 -- 595. Big Countries
 SELECT name, population, area
 FROM World
 WHERE area >= 3000000 OR population >=  25000000
+
+-- Day 4
+-- 
