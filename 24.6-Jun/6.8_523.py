96411993650adad70df23bc9996114b6839b1b81
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-08 11:37:28 -0700

    add daily algo

---
+++ 24.6-Jun/6.8_523.py
@@ -0,0 +1,24 @@
+"""
+    523. Continuous Subarray Sum
+    https://leetcode.com/problems/continuous-subarray-sum/
+
+    Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+    A good subarray is a subarray where:
+
+        • its length is at least two, and
+        • the sum of the elements of the subarray is a multiple of k.
+
+    Note that:
+
+        • A subarray is a contiguous part of the array.
+        • An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pass
