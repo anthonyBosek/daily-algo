9a30d1a3717dff6d6ac642667a0978598239b94f
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-10 15:37:25 -0700

    add algo solution

--- 24.6-Jun/6.10_1051.py
+++ 24.6-Jun/6.10_1051.py
@@ -18,4 +18,44 @@ from typing import List
 
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        pass
+        expected = sorted(heights)
+        return sum(1 for i in range(len(heights)) if heights[i] != expected[i])
+
+        #! ---------------------------------------------------------------
+
+        # c = heights[:]
+        # d = 0
+        # c.sort()
+        # for i in range(len(c)):
+        #     if heights[i] != c[i]:
+        #         d += 1
+        # return d
+
+        #! ---------------------------------------------------------------
+
+        # #? Function to perform bubble sort on the input array.
+        # def bubble_sort():
+        #     n = len(sorted_heights)
+        #     #? Loop through the array for bubble sort passes.
+        #     for i in range(n - 1):
+        #         #? Inner loop to compare and swap elements.
+        #         for j in range(n - i - 1):
+        #             #? Compare and swap if elements are in the wrong order.
+        #             if sorted_heights[j] > sorted_heights[j + 1]:
+        #                 sorted_heights[j], sorted_heights[j + 1] = (
+        #                     sorted_heights[j + 1],
+        #                     sorted_heights[j],
+        #                 )
+
+        # #? Sort the array using bubble sort.
+        # sorted_heights = heights[:]
+        # bubble_sort()
+
+        # count = 0
+        # #? Loop through the original and sorted arrays.
+        # for i in range(len(sorted_heights)):
+        #     #? Increment count if elements at the same index differ.
+        #     if heights[i] != sorted_heights[i]:
+        #         count += 1
+        # #? Return the total count of differing elements.
+        # return count
