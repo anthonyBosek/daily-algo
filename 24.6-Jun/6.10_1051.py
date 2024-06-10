7b6eceade613daa4c31318f1259a42982f884985
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-10 15:30:39 -0700

    add daily setup

---
+++ 24.6-Jun/6.10_1051.py
@@ -0,0 +1,21 @@
+"""
+    1051. Height Checker
+    https://leetcode.com/problems/height-checker/
+
+    A school is trying to take an annual photo of all the students. The students are asked to stand in a
+    single file line in non-decreasing order by height. Let this ordering be represented by the integer
+    array expected where expected[i] is the expected height of the ith student in line.
+
+    You are given an integer array heights representing the current order that the students are standing in.
+    Each heights[i] is the height of the ith student in line (0-indexed).
+
+    Return the number of indices where heights[i] != expected[i].
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        pass
