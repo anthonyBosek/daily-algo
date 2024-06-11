cdd99c5936c37cf167921e4a00aeaff9829eeed3
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-11 13:58:07 -0700

    add daily algo

---
+++ 24.6-Jun/6.11_1122.py
@@ -0,0 +1,17 @@
+"""
+    1122. Relative Sort Array
+    https://leetcode.com/problems/relative-sort-array/
+
+    Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+    Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+    Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        pass
