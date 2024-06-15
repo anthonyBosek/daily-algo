92edb22096b25e13c16caab0d168c11feb214d8a
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-15 11:39:34 -0700

    add SQL50 solutions

--- SQL50/last_25.sql
+++ SQL50/last_25.sql
@@ -6,4 +6,23 @@ GROUP BY 1
 HAVING COUNT(class) >= 5 
 
 -- Day 27
--- 
+-- 1729. Find Followers Count
+SELECT user_id, COUNT(*) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
+
+-- Day 28
+-- 619. Biggest Single Number
+SELECT MAX(num) AS num 
+FROM ( SELECT num
+        FROM MyNumbers
+        GROUP BY num
+        HAVING COUNT(num) = 1) AS a;
+
+-- Day 29
+-- 1045. Customers Who Bought All Products
+SELECT customer_id
+FROM customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT(product_key)) = (SELECT COUNT(*) FROM product)
