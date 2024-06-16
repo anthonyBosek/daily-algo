33041888c0787477faf106cd3431b7e2663a5b1b
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-16 11:19:57 -0700

    add daily challenge

---
+++ 24.6-Jun/6.16_330.py
@@ -0,0 +1,17 @@
+"""
+    330. Patching Array
+    https://leetcode.com/problems/patching-array/
+
+    Given a sorted integer array nums and an integer n, add/patch elements to the array such that
+    any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
+
+    Return the minimum number of patches required.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        pass
