5c7e51faf8e867965a4118755a7331887e4b0ceb
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-17 09:23:43 -0700

    add daily algo

---
+++ 24.6-Jun/6.17_633.py
@@ -0,0 +1,12 @@
+"""
+    633. Sum of Square Numbers
+    https://leetcode.com/problems/sum-of-square-numbers/
+
+    Given a non-negative integer c, decide whether there're two integers a and b such that a^2 + b^2 = c.
+
+"""
+
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        pass
