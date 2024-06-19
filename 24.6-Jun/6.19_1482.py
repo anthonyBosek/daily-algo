3405420423eed68bf878520d73a7211bba02d949
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-19 12:49:00 -0700

    add daily algo

---
+++ 24.6-Jun/6.19_1482.py
@@ -0,0 +1,25 @@
+"""
+    1482. Minimum Number of Days to Make m Bouquets
+    https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+
+    You are given an integer array bloomDay, an integer m and an integer k.
+
+    You want to make m bouquets. To make a bouquet, you need to use k adjacent flowers from the garden.
+
+    The garden consists of n flowers, the ith flower will bloom in the bloomDay[i] and then can be used
+    in exactly one bouquet.
+
+    Return the minimum number of days you need to wait to be able to make m bouquets from the garden.
+    If it is impossible to make m bouquets return -1.
+
+"""
+
+
+def minDays(bloomDay, k, m):
+    """
+    :type bloomDay: List[int]
+    :type m: int
+    :type k: int
+    :rtype: int
+    """
+    pass
