374a8d8da7e2ea64f0833fa6b9e47253e0e1085b
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-22 11:58:58 -0700

    add daily SQL50

--- SQL50/last_25.sql
+++ SQL50/last_25.sql
@@ -373,3 +373,43 @@ FROM all_ids
 GROUP BY id
 ORDER BY COUNT(id) DESC
 LIMIT 1;
+
+-- Day 43
+-- 185. Department Top Three Salaries
+-- *Approach 1: Window Function*
+SELECT d.name AS 'Department', 
+       e1.name AS 'Employee', 
+       e1.salary AS 'Salary' 
+FROM Employee e1
+JOIN Department d
+ON e1.departmentId = d.id 
+WHERE
+    3 > (SELECT COUNT(DISTINCT e2.salary)
+        FROM Employee e2
+        WHERE e2.salary > e1.salary AND e1.departmentId = e2.departmentId);
+-- *Approach 2: CTE*
+WITH employee_department AS
+    (
+    SELECT d.id, 
+        d.name AS Department, 
+        salary AS Salary, 
+        e.name AS Employee, 
+        DENSE_RANK()OVER(PARTITION BY d.id ORDER BY salary DESC) AS rnk
+    FROM Department d
+    JOIN Employee e
+    ON d.id = e.departmentId
+    )
+SELECT Department, Employee, Salary
+FROM employee_department
+WHERE rnk <= 3;
+
+-- Day 44
+-- 1667. Fix Names in a Table
+SELECT
+  user_id,
+  CONCAT(
+    UPPER(LEFT(name, 1)),
+    LCASE(SUBSTRING(name, 2))
+  ) AS name
+FROM Users
+ORDER BY user_id;
