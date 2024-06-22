4dd44f41e9bbb95a1e9833fa305013cc6b5cf89f
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-22 12:01:25 -0700

    add daily algo

---
+++ 24.6-Jun/6.22_1248.py
@@ -0,0 +1,18 @@
+"""
+    1248. Count Number of Nice Subarrays
+    https://leetcode.com/problems/count-number-of-nice-subarrays/
+
+    Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+    Return the number of nice sub-arrays.
+
+"""
+
+
+def numberOfSubarrays(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    pass
