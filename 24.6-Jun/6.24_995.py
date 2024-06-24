8eda5d9db9ec263644cfd7f71e27be95fbaf9b9b
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-24 05:40:08 -0700

    add daily algo

---
+++ 24.6-Jun/6.24_995.py
@@ -0,0 +1,22 @@
+"""
+    995. Minimum Number of K Consecutive Bit Flips
+    https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/
+
+    You are given a binary array nums and an integer k.
+
+    A k-bit flip is choosing a subarray of length k from nums and simultaneously changing every 0
+    in the subarray to 1, and every 1 in the subarray to 0.
+
+    Return the minimum number of k-bit flips required so that there is no 0 in the array.
+    If it is not possible, return -1.
+
+    A subarray is a contiguous part of an array.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        pass
