e1eff3eec9a0fb89143e4d1ccf2177bc0c3e8ac4
Anthony Bosek <anthony.bosek@gmail.com>
2024-06-24 05:38:31 -0700

    add daily SQL50

--- SQL50/last_10.sql
+++ SQL50/last_10.sql
@@ -103,7 +103,19 @@ SELECT
     ) AS SecondHighestSalary;
 
 -- Day 48
--- 
+-- 1484. Group Sold Products By The Date
+select 
+    sell_date,
+    count(distinct product) as num_sold,
+    group_concat(distinct product order by product) as products
+from
+    Activities 
+group by 
+    sell_date
+order by
+    sell_date;
+-- ap
+-- select sell_date, count(distinct product) as num_sold, Group_concat(distinct product separator ',') as products from Activities group by sell_date;
 
 -- Day 49
 -- 
