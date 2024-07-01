c5d16d7231d764b02d6b91d97f626f7561146344
Anthony Bosek <anthony.bosek@gmail.com>
2024-07-01 12:29:51 -0700

    add daily algo

---
+++ 24.7-Jul/7.1_1550.py
@@ -0,0 +1,15 @@
+"""
+    1550. Three Consecutive Odds
+    https://leetcode.com/problems/three-consecutive-odds/
+
+    Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+"""
+
+
+def threeConsecutiveOdds(arr):
+    """
+    :type arr: List[int]
+    :rtype: bool
+    """
+    pass
