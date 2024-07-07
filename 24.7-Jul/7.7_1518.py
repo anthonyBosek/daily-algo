187e36910de148f45f3fe7d7e86660c031208172
Anthony Bosek <anthony.bosek@gmail.com>
2024-07-07 09:29:45 -0700

    add daily algo

---
+++ 24.7-Jul/7.7_1518.py
@@ -0,0 +1,21 @@
+"""
+    1518. Water Bottles
+    https://leetcode.com/problems/water-bottles/
+
+    There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water
+    bottles from the market with one full water bottle.
+
+    The operation of drinking a full water bottle turns it into an empty bottle.
+
+    Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
+"""
+
+
+def numWaterBottles(numBottles, numExchange):
+    """
+    :type numBottles: int
+    :type numExchange: int
+    :rtype: int
+    """
+    pass
