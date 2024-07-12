f6723aa3ff48e7ac96b898fca2d6fb9bcc5b4d5d
Anthony Bosek <anthony.bosek@gmail.com>
2024-07-12 13:57:07 -0700

    add daily

---
+++ 24.7-Jul/7.12_1717.py
@@ -0,0 +1,25 @@
+"""
+    1717. Maximum Score From Removing Substrings
+    https://leetcode.com/problems/maximum-score-from-removing-substrings/
+
+    You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+        1. Remove substring "ab" and gain x points.
+        For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+
+        2. Remove substring "ba" and gain y points.
+        For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+
+    Return the maximum points you can gain after applying the above operations on s.
+
+"""
+
+
+def maximumGain(s, x, y):
+    """
+    :type s: str
+    :type x: int
+    :type y: int
+    :rtype: int
+    """
+    pass
