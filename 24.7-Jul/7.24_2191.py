cd143277d514a52fae5962effd1ec63133501b78
Anthony Bosek <anthony.bosek@gmail.com>
2024-07-24 12:02:30 -0700

    add solution

--- 24.7-Jul/7.24_2191.py
+++ 24.7-Jul/7.24_2191.py
@@ -24,4 +24,63 @@ from typing import List
 
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        pass
+        # return sorted(
+        #     nums,
+        #     key=lambda n: int("".join(map(lambda d: str(mapping[int(d)]), str(n)))),
+        # )
+        # #!
+        trans_rule = str.maketrans({str(i): str(x) for i, x in enumerate(mapping)})
+        return sorted(nums, key=lambda x: int(str(x).translate(trans_rule)))
+
+
+#! Approach 1: Conversion using strings and Sorting
+class Solution:
+    def sortJumbled(self, mapping, nums):
+        store_pairs = []
+
+        for i in range(len(nums)):
+            # Convert current value to string
+            number = str(nums[i])
+            formed = ""
+            for j in range(len(number)):
+                formed = formed + str(mapping[int(number[j])])
+            # Store the mapped value.
+            mapped_value = int(formed)
+            # Push a pair consisting of mapped value and original value's index.
+            store_pairs.append((mapped_value, i))
+
+        # Sort the array in non-decreasing order by the first value (default).
+        store_pairs.sort()
+        answer = []
+        for pair in store_pairs:
+            answer.append(nums[pair[1]])
+        return answer
+
+
+#! Approach 2: Conversion without using strings and Sorting
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        store_pairs = []
+
+        for i in range(len(nums)):
+            mapped_value = 0
+            temp = nums[i]
+
+            # Start making changes from the units place.
+            place = 1
+            # If the value initially is 0, return mapping[0] and index.
+            if temp == 0:
+                store_pairs.append((mapping[0], i))
+                continue
+            # Repeat the process for units, tenths, hundredths.. places.
+            while temp != 0:
+                mapped_value = place * mapping[temp % 10] + mapped_value
+                place *= 10
+                temp //= 10
+            store_pairs.append((mapped_value, i))
+
+        # Sort the array in non-decreasing order by the first value (default).
+        store_pairs.sort()
+        answer = [nums[pair[1]] for pair in store_pairs]
+
+        return answer
