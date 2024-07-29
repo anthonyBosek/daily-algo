c7a7e37ecac4d65bda4937f30d4745858f28f13c
Anthony Bosek <anthony.bosek@gmail.com>
2024-07-29 13:41:33 -0700

    add algo

---
+++ 24.7-Jul/7.29_1395.py
@@ -0,0 +1,22 @@
+"""
+    1395. Count Number of Teams
+    https://leetcode.com/problems/count-number-of-teams/
+
+    There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+
+    You have to form a team of 3 soldiers amongst them under the following rules:
+
+        • Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+        • A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+
+    Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+"""
+
+
+def numTeams(rating):
+    """
+    :type rating: List[int]
+    :rtype: int
+    """
+    pass
