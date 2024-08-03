a6f09d80627c6f2ae1613134cb4142b4e06a594b
Anthony Bosek <anthony.bosek@gmail.com>
2024-08-03 07:25:52 -0700

    create daily branch

---
+++ 24.8-Aug/8.3_1460.py
@@ -0,0 +1,15 @@
+"""
+    1460. Make Two Arrays Equal by Reversing Subarrays
+    https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/
+
+    You are given two integer arrays of equal length target and arr. In one step,
+    you can select any non-empty subarray of arr and reverse it. You are allowed
+    to make any number of steps.
+
+    Return true if you can make arr equal to target or false otherwise.
+
+"""
+
+
+def canBeEqual(target, arr):
+    pass
