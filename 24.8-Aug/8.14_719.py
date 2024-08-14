8f5303ed1c0159ca5ad784ec740f0540cf82dcf7
Anthony Bosek <anthony.bosek@gmail.com>
2024-08-14 06:35:29 -0700

    add daily

---
+++ 24.8-Aug/8.14_719.py
@@ -0,0 +1,16 @@
+"""
+    719. Find K-th Smallest Pair Distance
+    https://leetcode.com/problems/find-k-th-smallest-pair-distance/
+
+    The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+
+    Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        pass
