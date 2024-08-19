6744299f3396a0f005f626512c2756c9a9b83712
Anthony Bosek <anthony.bosek@gmail.com>
2024-08-19 06:37:23 -0700

    add algo

---
+++ 24.8-Aug/8.19_650.py
@@ -0,0 +1,20 @@
+"""
+    650. 2 Keys Keyboard
+    https://leetcode.com/problems/2-keys-keyboard/
+
+    There is only one character 'A' on the screen of a notepad. You can perform one of two operations on this notepad for each step:
+
+        • Copy All: You can copy all the characters present on the screen (a partial copy is not allowed).
+        • Paste: You can paste the characters which are copied last time.
+
+    Given an integer n, return the minimum number of operations to get the character 'A' exactly n times on the screen.
+
+"""
+
+
+def minSteps(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    pass
