6103c6f060aa183669e0b80fe3d1d466285b7b67
Anthony Bosek <anthony.bosek@gmail.com>
2024-08-21 06:27:51 -0700

    add daily

---
+++ 24.8-Aug/8.21_664.py
@@ -0,0 +1,21 @@
+"""
+    664. Strange Printer
+    https://leetcode.com/problems/strange-printer/
+
+    There is a strange printer with the following two special requirements:
+
+        - The printer can only print a sequence of the same character each time.
+        - At each turn, the printer can print new characters starting from and ending
+            at any place and will cover the original existing characters.
+
+    Given a string s, return the minimum number of turns the printer needed to print it.
+
+"""
+
+
+def strangePrinter(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    pass
