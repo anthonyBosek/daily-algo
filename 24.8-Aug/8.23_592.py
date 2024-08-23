0814d16c195a4658cdcbca23d832692b9396cfc9
Anthony Bosek <anthony.bosek@gmail.com>
2024-08-23 07:20:56 -0700

    add solution

--- 24.8-Aug/8.23_592.py
+++ 24.8-Aug/8.23_592.py
@@ -12,9 +12,145 @@
 """
 
 
-def fractionAddition(expression):
-    """
-    :type expression: str
-    :rtype: str
-    """
-    pass
+class Solution:
+    def fractionAddition(self, expression):
+        num = 0
+        denom = 1
+
+        i = 0
+        while i < len(expression):
+            curr_num = 0
+            curr_denom = 0
+
+            is_negative = False
+
+            # check for sign
+            if expression[i] == "-" or expression[i] == "+":
+                if expression[i] == "-":
+                    is_negative = True
+                # move to next character
+                i += 1
+
+            # build numerator
+            while i < len(expression) and expression[i].isdigit():
+                val = int(expression[i])
+                curr_num = curr_num * 10 + val
+                i += 1
+
+            if is_negative:
+                curr_num *= -1
+
+            # skip divisor
+            i += 1
+
+            # build denominator
+            while i < len(expression) and expression[i].isdigit():
+                val = int(expression[i])
+                curr_denom = curr_denom * 10 + val
+                i += 1
+
+            # add fractions together using common denominator
+            num = num * curr_denom + curr_num * denom
+            denom = denom * curr_denom
+
+        gcd = abs(self._find_gcd(num, denom))
+
+        # reduce fractions
+        num //= gcd
+        denom //= gcd
+
+        return f"{num}/{denom}"
+
+    def _find_gcd(self, a, b):
+        if a == 0:
+            return b
+        return self._find_gcd(b % a, a)
+
+
+#! Approach 1: Manual Parsing + Common Denominator
+class Solution:
+    def fractionAddition(self, expression):
+        num = 0
+        denom = 1
+
+        i = 0
+        while i < len(expression):
+            curr_num = 0
+            curr_denom = 0
+
+            is_negative = False
+
+            # check for sign
+            if expression[i] == "-" or expression[i] == "+":
+                if expression[i] == "-":
+                    is_negative = True
+                # move to next character
+                i += 1
+
+            # build numerator
+            while i < len(expression) and expression[i].isdigit():
+                val = int(expression[i])
+                curr_num = curr_num * 10 + val
+                i += 1
+
+            if is_negative:
+                curr_num *= -1
+
+            # skip divisor
+            i += 1
+
+            # build denominator
+            while i < len(expression) and expression[i].isdigit():
+                val = int(expression[i])
+                curr_denom = curr_denom * 10 + val
+                i += 1
+
+            # add fractions together using common denominator
+            num = num * curr_denom + curr_num * denom
+            denom = denom * curr_denom
+
+        gcd = abs(self._find_gcd(num, denom))
+
+        # reduce fractions
+        num //= gcd
+        denom //= gcd
+
+        return f"{num}/{denom}"
+
+    def _find_gcd(self, a, b):
+        if a == 0:
+            return b
+        return self._find_gcd(b % a, a)
+
+
+#! Approach 2 - Parsing with Regular Expressions
+import re
+
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        num = 0
+        denom = 1
+
+        # separate expression into signed numbers
+        nums = re.split("/|(?=[-+])", expression)
+        nums = list(filter(None, nums))
+
+        for i in range(0, len(nums), 2):
+            curr_num = int(nums[i])
+            curr_denom = int(nums[i + 1])
+
+            num = num * curr_denom + curr_num * denom
+            denom = denom * curr_denom
+
+        gcd = abs(self._find_gcd(num, denom))
+
+        num //= gcd
+        denom //= gcd
+
+        return str(num) + "/" + str(denom)
+
+    def _find_gcd(self, a: int, b: int) -> int:
+        if a == 0:
+            return b
+        return self._find_gcd(b % a, a)
