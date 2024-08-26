932e573c49843c163d5d20ab283e67e06fecce0c
Anthony Bosek <anthony.bosek@gmail.com>
2024-08-26 06:08:44 -0700

    add daily algo

---
+++ 24.8-Aug/8.26_590.py
@@ -0,0 +1,23 @@
+"""
+    590. N-ary Tree Postorder Traversal
+    https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+    Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+
+    Nary-Tree input serialization is represented in their level order traversal.
+    Each group of children is separated by the null value (See examples)
+
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def postorder(self, root: "Node") -> List[int]:
+        pass
