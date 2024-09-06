0bbbcd22eea6e6ddbb756a4fe9ca6cb4a59c6b03
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-06 06:30:01 -0700

    add daily

---
+++ 24.9-Sept/9.6_3217.py
@@ -0,0 +1,24 @@
+"""
+    3217. Delete Nodes From Linked List Present in Array
+    https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/
+
+    You are given an array of integers nums and the head of a linked list. Return the head of the modified
+    linked list after removing all nodes from the linked list that have a value that exists in nums.
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def modifiedList(self, nums, head):
+        """
+        :type nums: List[int]
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pass
