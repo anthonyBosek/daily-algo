ae56345d6c5730752e414d3b48e19daddd0ff1f5
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-13 06:19:19 -0700

    add algo

---
+++ 24.9-Sept/9.13_1310.py
@@ -0,0 +1,21 @@
+"""
+    1310. XOR Queries of a Subarray
+    https://leetcode.com/problems/xor-queries-of-a-subarray/
+
+    You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+
+    For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+
+    Return an array answer where answer[i] is the answer to the ith query.
+
+"""
+
+
+class Solution:
+    def xorQueries(self, arr, queries):
+        """
+        :type arr: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        pass
