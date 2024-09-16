b89375640cda133a2bf231b89c6eb9bca7a14065
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-16 06:33:43 -0700

    add daily

---
+++ 24.9-Sept/9.16_539.py
@@ -0,0 +1,17 @@
+"""
+    539. Minimum Time Difference
+    https://leetcode.com/problems/minimum-time-difference/
+
+    Given a list of 24-hour clock time points in "HH:MM" format, return the minimum
+    time difference (in minutes) between any two time points in the list.
+
+"""
+
+
+class Solution:
+    def findMinDifference(self, timePoints):
+        """
+        :type timePoints: List[str]
+        :rtype: int
+        """
+        pass
