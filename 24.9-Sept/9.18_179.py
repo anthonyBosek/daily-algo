25bb13923d15a9c62189ce187f7b904993d6ddfa
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-18 05:38:15 -0700

    add daily algo

---
+++ 24.9-Sept/9.18_179.py
@@ -0,0 +1,13 @@
+"""
+    179. Largest Number
+    https://leetcode.com/problems/largest-number/
+
+    Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+    Since the result may be very large, you will need to return a string instead of an integer.
+    
+"""
+
+
+def largestNumber(nums):
+    pass
