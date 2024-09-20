be128b2b8b015dd9431e035bca70c890016f559a
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-20 05:43:59 -0700

    add daily

---
+++ 24.9-Sept/9.20_214.py
@@ -0,0 +1,17 @@
+"""
+    214. Shortest Palindrome
+    https://leetcode.com/problems/shortest-palindrome/
+
+    You are given a string s. You can convert s to a palindrome by adding characters in front of it.
+
+    Return the shortest palindrome you can find by performing this transformation.
+    
+"""
+
+
+def shortestPalindrome(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    pass
