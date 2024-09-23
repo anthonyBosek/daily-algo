5bd41ff78894adbfdf4e452c19dc60a6a88040b3
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-23 05:39:36 -0700

    add daily

---
+++ 24.9-Sept/9.23_2707.py
@@ -0,0 +1,21 @@
+"""
+    2707. Extra Characters in a String
+    https://leetcode.com/problems/extra-characters-in-a-string/
+
+    You are given a 0-indexed string s and a dictionary of words dictionary.
+    You have to break s into one or more non-overlapping substrings such that each substring
+    is present in dictionary. There may be some extra characters in s which are not present
+    in any of the substrings.
+
+    Return the minimum number of extra characters left over if you break up s optimally.
+
+"""
+
+
+def minExtraChar(s, dictionary):
+    """
+    :type s: str
+    :type dictionary: List[str]
+    :rtype: int
+    """
+    pass
