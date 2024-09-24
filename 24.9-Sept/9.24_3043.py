3e709f2a62c358423f477e6dca0ce279805f2df0
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-24 06:21:17 -0700

    add daily challenge

---
+++ 24.9-Sept/9.24_3043.py
@@ -0,0 +1,26 @@
+"""
+    3043. Find the Length of the Longest Common Prefix
+    https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/
+
+    You are given two arrays with positive integers arr1 and arr2.
+
+    A prefix of a positive integer is an integer formed by one or more of its digits,
+    starting from its leftmost digit. For example, 123 is a prefix of the integer 12345,
+    while 234 is not.
+
+    A common prefix of two integers a and b is an integer c, such that c is a prefix of both a and b.
+    For example, 5655359 and 56554 have a common prefix 565 while 1223 and 43456 do not have a common prefix.
+
+    You need to find the length of the longest common prefix between all pairs of integers (x, y)
+    such that x belongs to arr1 and y belongs to arr2.
+
+    Return the length of the longest common prefix among all pairs. If no common prefix exists among them, return 0.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        pass
