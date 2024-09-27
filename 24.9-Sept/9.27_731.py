9178c68dad6a3b8beefb2b3700b41f60f14bb575
Anthony Bosek <anthony.bosek@gmail.com>
2024-09-27 06:29:20 -0700

    add solution

--- 24.9-Sept/9.27_731.py
+++ 24.9-Sept/9.27_731.py
@@ -25,12 +25,93 @@
 class MyCalendarTwo:
 
     def __init__(self):
-        pass
+        self.calendar = []
+        self.overlaps = []
 
     def book(self, start: int, end: int) -> bool:
-        pass
+        for i, j in self.overlaps:
+            if start < j and end > i:
+                return False
+
+        for i, j in self.calendar:
+            if start < j and end > i:
+                self.overlaps.append((max(start, i), min(end, j)))
+
+        self.calendar.append((start, end))
+        return True
 
 
 # Your MyCalendarTwo object will be instantiated and called as such:
 # obj = MyCalendarTwo()
 # param_1 = obj.book(start,end)
+
+
+#! Approach 1: Using Overlapped Intervals
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.bookings = []
+        self.overlap_bookings = []
+
+    def book(self, start: int, end: int) -> bool:
+        # Check if the new booking overlaps with any double-booked booking.
+        for booking in self.overlap_bookings:
+            if self.does_overlap(booking[0], booking[1], start, end):
+                return False
+
+        # Add any new double overlaps that the current booking creates.
+        for booking in self.bookings:
+            if self.does_overlap(booking[0], booking[1], start, end):
+                self.overlap_bookings.append(
+                    self.get_overlapped(booking[0], booking[1], start, end)
+                )
+
+        # Add the new booking to the list of bookings.
+        self.bookings.append((start, end))
+        return True
+
+    # Return True if the booking [start1, end1) & [start2, end2) overlaps.
+    def does_overlap(self, start1: int, end1: int, start2: int, end2: int) -> bool:
+        return max(start1, start2) < min(end1, end2)
+
+    # Return the overlapping booking between [start1, end1) & [start2, end2).
+    def get_overlapped(self, start1: int, end1: int, start2: int, end2: int) -> tuple:
+        return max(start1, start2), min(end1, end2)
+
+
+#! Approach 2: Line Sweep
+from sortedcontainers import SortedDict
+
+
+class MyCalendarTwo:
+
+    def __init__(self):
+        # Store the number of bookings at each point.
+        self.booking_count = SortedDict()
+        # The maximum number of overlapped bookings allowed.
+        self.max_overlapped_booking = 2
+
+    def book(self, start: int, end: int) -> bool:
+        # Increase and decrease the booking count at the start and end respectively.
+        self.booking_count[start] = self.booking_count.get(start, 0) + 1
+        self.booking_count[end] = self.booking_count.get(end, 0) - 1
+
+        overlapped_booking = 0
+
+        # Calculate the prefix sum of bookings.
+        for count in self.booking_count.values():
+            overlapped_booking += count
+            # If the number of overlaps exceeds the allowed limit
+            # rollback and return False.
+            if overlapped_booking > self.max_overlapped_booking:
+                # Rollback changes.
+                self.booking_count[start] -= 1
+                self.booking_count[end] += 1
+
+                # Remove entries if their count becomes zero to clean up the SortedDict.
+                if self.booking_count[start] == 0:
+                    del self.booking_count[start]
+
+                return False
+
+        return True
